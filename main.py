d984ecb65a96577b17beff0842234815cbd5a4ef
= <=>
2016-11-02 20:51:39 +0100

    landing page

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#@app.route('/')
+#def index():
+#	return render_template('_index.html')
+
+
 @app.route('/')
 def index():
 	return render_template('index.html')
